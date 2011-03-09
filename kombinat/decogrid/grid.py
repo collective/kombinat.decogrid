f355c8ca41d3f056534a9ee5f8018fb0ca4ab515
Daniel Widerin <daniel@widerin.net>
2011-03-09 20:58:07 +0000

    build a class

--- kombinat/decogrid/grid.py
+++ kombinat/decogrid/grid.py
@@ -1,95 +1,134 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import optparse
-import os
+import os.path
+import string
+import sys
 
-cell = 0.1
-while int(cell) != cell:
-    width = int(raw_input("Portal width [960]: ") or 960)
-    padding = int(raw_input("Global margin between columns [10]: ") or 10)
-    columns = int(raw_input("Columns [16]: ") or 16)
-    lrm = (raw_input("Use left/right margin [Y/n]: ").lower() or 'y') == 'y'
 
-    if lrm:
-        # when using a left/right margin it would be
-        # n-1 * padding + 2 * 0.5 * padding = n * padding
-        cell = float(width - columns * padding) / columns
-    else:
-        cell = float(width - (columns - 1) * padding) / columns
+class Deco(object):
 
-    if int(cell) != cell:
-        print "\n\nWARNING: You won't get valid cell width pixel entries."
-        if (raw_input("Go on, don't ask me! [y/N]: ").lower() or 'n') == 'y':
-            break
+    element = "div"
+    css_row = "row"
+    css_cell = "cell"
+    css_width = "width"
+    css_position = "position"
 
-css = """
+    template = string.Template(open(
+        os.path.join(os.path.dirname(__file__), 'deco.css'), 'r').read())
 
-/* The %d-column Deco Grid System for fixed width of %dpx:
- * Cells are %dpx (%.2f%%), margin will be %dpx (%.2f%%).
- * Portal left/right margin is %dpx (%.2f%%).
- */
+    def __init__(self, width, margin, columns, omit):
+        self.width = width
+        self.margin = margin
+        self.columns = columns
+        self.omit = omit
 
-div.row {
-  float: left;
-  width: 100%%;
-  display: block;
-  position: relative;
-}
-div.cell {
-  position: relative;
-  float: left;
-  left: 100%%;
-}
+    @property
+    def p_cell(self):
+        return 100.0 / self.width * self.cell
 
-/* Width classes. */ """ % (columns, width,
-                            cell, 100.0 / width * cell,
-                            padding, 100.0 / width * padding,
-                            lrm and padding / 2.0 or 0,
-                            lrm and 100.0 / width * (padding / 2.0) or 0)
+    @property
+    def p_margin(self):
+        return 100.0 / self.width * self.margin
 
-# width
-for i in range(1, columns + 1):
-    css += "\ndiv.width-%d { width: %.4f%%; }" % (
-        i, 100.0 / width * (i * cell + (i - 1) * padding))
+    @property
+    def portal_margin(self):
+        if self.omit:
+            return 0
+        return self.margin / 2.0
 
-# positioning
-css += """
+    @property
+    def p_portal_margin(self):
+        return 100.0 / self.width * self.portal_margin
 
-/* Positioning classes, these are subtracting from a rightmost position, which
-is why they seem the wrong way around */"""
-for i in range(0, columns):
-    css += "\ndiv.position-%d { margin-left: -%.4f%%; }" % (
-        i, 100 - (100.0 / width * (lrm and (i * cell + (i + 0.5) * padding) \
-                                       or (i * (cell + padding)))))
+    @property
+    def cell(self):
+        if self.omit:
+            margin = (self.columns - 1) * self.margin
+        else:
+            margin = self.columns * self.margin
 
-# convenience classes
-css += """
-/* End of the core Deco Grid System */
+        return float(self.width - margin) / self.columns
 
-/* Convenience classes. Not strictly necessary. */
-div.width-full { width: %.4f%% }
-div.position-0 { margin-left: -%.4f%% }
-""" % (100.0 / width * (width - (lrm and padding or 0)),
-       100.0 / width * (width - (lrm and padding * 0.5 or 0)))
+    @property
+    def width_full(self):
+        if self.omit:
+            return 100
 
-for i in range(2, min(7, columns)):
-    pos = ""
-    for j in range(1, i):
+        return 100.0 / self.width * (self.width - self.margin)
 
-        # calculate new cell width
-        cell = float(width - (i - (not lrm and 1 or 0)) * padding) / i
+    @property
+    def position_0(self):
+        if self.omit:
+            return - 100
 
-        css += "\ndiv.width-%d\\3a %d { width: %.4f%% }" % (
-            j, i, 100.0 / width * \
-                          (j * cell + (j - 1) * padding))
+        return 100.0 / self.width * (self.width - (self.margin * 0.5))
 
-        pos += "\ndiv.position-%d\\3a %d { margin-left: -%.4f%% }" % (
-            j, i, 100 - (100.0 / width * \
-                         (j * cell + (j + (lrm and 0.5 or 0)) * padding)))
+    def widths(self):
+        css = ""
+        for i in range(1, self.columns + 1):
+            w = 100.0 / self.width * (i * self.cell + (i - 1) * self.margin)
+            css += "\n%s.%s-%d { width: %.4f%%; }" % (
+                self.element, self.css_width, i, w)
 
-    css += pos + "\n"
+        return css
 
-print css
+    def positions(self):
+        css = ""
+        for i in range(0, self.columns):
+            if self.omit:
+                p = -100 + 100.0 / self.width * \
+                    i * (self.cell + self.margin)
+            else:
+                p = -100 + 100.0 / self.width * \
+                    (i * self.cell + (i + 0.5) * self.margin)
+
+            css += "\n%s.%s-%d { margin-left: %.4f%%; }" % (
+                self.element, self.css_position, i, p)
+
+        return css
+
+    def conveniences(self, level=7):
+        css = """\
+%s.%s-full { width: %.4f%% }
+%s.%s-0 { margin-left: %.4f%% }
+""" % (self.element, self.css_width, self.width_full,
+       self.element, self.css_position, self.position_0)
+
+        for i in range(2, min(level, self.columns)):
+            pos = ""
+            for j in range(1, i):
+
+                if self.omit:
+                    cell = float(self.width - (i - 1) * self.margin) / i
+                else:
+                    cell = float(self.width - self.margin * i) / i
+
+                css += "\n%s.%s-%d\\3a %d { width: %.4f%% }" % (
+                    self.element, self.css_width, j, i,
+                    100.0 / self.width * (j * cell + (j - 1) * self.margin))
+
+                pos += "\n%s.%s-%d\\3a %d { margin-left: %.4f%% }" % (
+                    self.element, self.css_position, j, i, -100 + (
+                        100.0 / self.width * (j * cell + (j + (
+                            not self.omit and 0.5 or 0)) * self.margin)))
+
+            css += pos + "\n"
+
+        return css
+
+    def __getitem__(self, key):
+        value = getattr(self, key)
+        if callable(value):
+            return value()
+
+        return value
+
+    def __call__(self):
+        if int(self.cell) != self.cell:
+            sys.stdout.write("/* WARN: You don't have int pixel values.*/\n\n")
+
+        return self.template.substitute(self)
 
 
 def generator():
@@ -97,28 +136,33 @@ def generator():
 
 %prog generate decogrid columns.css for plone (http://deco.gs).""")
 
-    parser.add_option('-w', '--width', action='store_true',
-                      dest='width', default=960, type="int",
-                      help="portal width in pixels incl. potentially margins")
-
-    parser.add_option('-m', '--margin', action='store_true',
-                      dest='margin', default=10, type="int",
-                      help="Margins between cells.")
-
-    parser.add_option('-c', '--columns', action='store_true',
-                      dest='columns', default=16, type="int",
-                      help="Columns.")
+    parser.add_option('-c', '--columns', dest='columns', default=16,
+                      type="int", metavar="COLUMNS",
+                      help="how many columns (default: 16)")
 
     parser.add_option('-o', '--omit-margin', action='store_true',
-                      dest='omit', default=False, type="bool",
-                      help="Omit left and right margin around portal " + \
+                      dest='omit', default=False,
+                      help="omit left and right margin around the portal " + \
                            "(recommended for nesting grids inside grids).")
+
+    parser.add_option('-m', '--margin', dest='margin', default=10,
+                      type="int", metavar="MARGIN",
+                      help="margin in pixels between each cell (default 10)")
+
+    parser.add_option('-w', '--width', dest='width', default=960,
+                      type="int", metavar="WIDTH",
+                      help="portal width in pixels including potentially " + \
+                           "left and/or right margins (default: 960)")
 
     (options, args) = parser.parse_args()
 
     if len(args) != 0:
         parser.print_help()
         return
+
+    deco = Deco(options.width, options.margin, options.columns, options.omit)
+    sys.stdout.write(deco())
+
 
 if __name__ == '__main__':
     generator()
