392e69c5b56dc17a6bbc6c56082bcc1814ed3490
Daniel Widerin <daniel@widerin.net>
2012-09-24 21:07:18 +0200

    Move README.txt to README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.3'
 setup(name='kombinat.decogrid',
       version=version,
       description="A decogrid generator for plone columns.css file.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
